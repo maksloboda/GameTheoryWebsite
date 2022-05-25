f298c8036fc7bca89c872fe4d80fcccc8eded853
Max <maksloboda123@gmail.com>
2022-05-25 18:13:39 +0300

    Cleanup

--- Seki/api/api.cpp
+++ Seki/api/api.cpp
@@ -1,158 +1,12 @@
 #include <iostream>
 #include <api/json.hpp>
-#include <api/detail.hpp>
+#include <core/core.h>
 #include <tuple>
 #include <functional>
-#include <core/core.h>
-
-namespace nlohmann {
-  template <>
-  struct adl_serializer<core::GameState> {
-    static void to_json(nlohmann::json &json, const core::GameState &state) {
-      auto current_player = state.get_is_r() ? "R" : "C";
-      auto field_width = state.get_field().get_shape()[1];
-      auto field_height = state.get_field().get_shape()[0];
-
-      std::vector<int> v;
-      v.reserve(field_width * field_height);
-
-      for (int y = 0; y < field_height; ++y) {
-        for (int x = 0; x < field_width; ++x) {
-          v.push_back(state.get_field().get(x, y));
-        }
-      }
-
-      auto type = api::detail::match<core::SekiType, std::string, 2>(
-        {core::SekiType::SEKI, core::SekiType::DSEKI},
-        {"seki", "dseki"},
-        state.get_type());
-      
-      if (!type) {
-        throw std::runtime_error("Invalid game type");
-      }
-
-      auto passtype = api::detail::match<core::PassType, std::string, 4>(
-        {
-          core::PassType::R_PASS, core::PassType::C_PASS,
-          core::PassType::ANY_PASS, core::PassType::NO_PASS,
-        },
-        {"R", "C", "RC", ""},
-        state.get_passtype());
-
-      if (!passtype) {
-        throw std::runtime_error("Invalid pass type");
-      }
-
-      json = nlohmann::json({
-        {"CurrentPlayer", current_player},
-        {"Width", field_width},
-        {"Height", field_height},
-        {"FlattenedField", v},
-        {"Type", *type},
-        {"Pp", *passtype},
-      });
-    }
-
-    static core::GameState from_json(const nlohmann::json &json) {
-      auto current_player = json.at("CurrentPlayer").get<std::string>();
-      bool is_r = current_player == "R";
-      if (current_player != "R" && current_player != "C") {
-        throw std::runtime_error("Invalid player specifided");
-      }
-
-      auto width = json.at("Width").get<int>();
-      auto height = json.at("Height").get<int>();
-      if (width < 1 || height < 1
-        || static_cast<size_t>(width) > core::max_field_dim
-        || static_cast<size_t>(height) > core::max_field_dim) {
-        throw std::runtime_error("Invalid field size");
-      }
-
-      auto uwidth = static_cast<size_t>(width);
-      auto uheight = static_cast<size_t>(height);
-
-      auto flat_field_data = json.at("FlattenedField").get<std::vector<int>>();
-      if (flat_field_data.size() != uwidth * uheight) {
-        throw std::runtime_error("Field size mismatch");
-      }
-
-      std::vector<std::vector<int>> field_data(uheight, std::vector<int>(uwidth, 0));
-
-      for(size_t y = 0; y < uheight; ++y) {
-        for(size_t x = 0; x < uwidth; ++x) {
-          auto value = flat_field_data[x + y * uwidth];
-          if (value < 0) {
-            throw std::runtime_error("Negative items are not allowed");
-          }
-          field_data[y][x] = value;
-        }
-      }
-
-      auto type = api::detail::match<std::string, core::SekiType, 2>(
-        {"seki", "dseki"},
-        {core::SekiType::SEKI, core::SekiType::DSEKI},
-        json.at("Type").get<std::string>());
-      
-      if (!type) {
-        throw std::runtime_error("Invalid game type");
-      }
-
-      auto pass = api::detail::match<std::string, core::PassType, 4>(
-        {"R", "C", "RC", ""},
-        {
-          core::PassType::R_PASS, core::PassType::C_PASS,
-          core::PassType::ANY_PASS, core::PassType::NO_PASS,
-        },
-        json.at("Pp").get<std::string>());
-
-      if (!pass) {
-        throw std::runtime_error("Invalid pass type");
-      }
-
-      return core::GameState(
-        core::Field(field_data),
-        is_r,
-        1,
-        *type,
-        *pass);
-    }
-  };
-
-  template <>
-  struct adl_serializer<core::Move> {
-    static core::Move from_json(const nlohmann::json &json) {
-      int x = json.at("X").get<int>();
-      int y = json.at("Y").get<int>();
-      auto result = core::Move(0, x, y);
-      result.is_pass = json.at("IsPass").get<bool>();
-      return result;
-    }
-
-    static void to_json(nlohmann::json &json, const core::Move &m) {
-      json = nlohmann::json({
-        {"X", m.x},
-        {"Y", m.y},
-        {"IsPass", m.is_pass},
-      });
-    }
-  };
-
-}
+#include <api/detail.hpp>
+#include <api/serialization.hpp>
 
 namespace api {
-
-struct GameInfo {
-  std::string id;
-  std::vector<std::string> players_joined;
-  std::string game_name;
-  int event_clock;
-  std::string state;
-  bool is_ready;
-  bool is_finished;
-  std::string winner;
-  NLOHMANN_DEFINE_TYPE_INTRUSIVE(GameInfo, id, players_joined, game_name,
-      event_clock, state, is_ready, is_finished, winner)
-};
 
 bool is_start_state_valid(std::string enc_state) {
   auto state = nlohmann::json::parse(enc_state).get<core::GameState>();
@@ -194,21 +48,19 @@ GameInfo add_event(GameInfo gi, std::string pid, std::string mv_enc) {
       gi.state = nlohmann::json(state).dump();
       if (state.is_terminal()) {
         gi.is_finished = true;
-        switch (state.get_status())
-        {
-        case core::GameState::Status::R_won:
-          gi.winner = "R";
-          break;
-        case core::GameState::Status::C_won:
-          gi.winner = "C";
-          break;
-        case core::GameState::Status::Draw:
-          gi.winner = "";
-        break;
-        default:
+        auto winner = detail::match<core::GameState::Status, std::string, 3>(
+          {
+            core::GameState::Status::R_won,
+            core::GameState::Status::C_won,
+            core::GameState::Status::Draw,
+          },
+          {"R", "C", ""},
+          state.get_status()
+        );
+        if (!winner) {
           throw std::runtime_error("Status is broken");
         }
-        
+        gi.winner = *winner;
       }
       return gi;
     } else {
