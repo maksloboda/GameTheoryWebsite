f6aa12deda111e1e2ed2439590a7c178f3354e4b
Max <maksloboda123@gmail.com>
2022-05-30 18:42:44 +0300

    Draw support, Fixes

--- SingleSuit/main.py
+++ SingleSuit/main.py
@@ -11,10 +11,27 @@ def joinGame(gameinfo, pid):
     raise Exception("Player already in game")
   elif len(players_joined) == 2:
     raise Exception("Game is full")
-  players_joined.append(pid)
-  if len(players_joined) == 2:
+  if pid not in ["A", "B"]:
+    raise Exception("Unknown player")
+  if len(players_joined) == 1:
     gameinfo["is_ready"] = True
   return gameinfo
+
+def processFinished(gameinfo, state, leading, can_draw):
+  fps = state["FirstPlayerSet"]
+  sps = state["SecondPlayerSet"]
+  if len(fps) == 0 and len(sps) == 0:
+    if can_draw:
+      gameinfo["winner"] = ""
+    else:
+      gameinfo["winner"] = leading
+    gameinfo["is_finished"] = True
+  elif len(fps) == 0:
+    gameinfo["winner"] = "A"
+    gameinfo["is_finished"] = True
+  elif len(sps) == 0:
+    gameinfo["winner"] = "B"
+    gameinfo["is_finished"] = True
 
 def addEvent(gameinfo, pid, move_enc):
   players_joined = gameinfo["players_joined"]
@@ -26,14 +43,21 @@ def addEvent(gameinfo, pid, move_enc):
   move = json.loads(move_enc)
 
   should_swap = False
+  can_draw = state["Type"] == "d-singlesuit"
 
   if move["DoTake"] == True:
     last_card = state["LastCard"]
+
+    if last_card is None:
+      raise Exception("Cant take nothing")
+
     should_swap = True
     if pid == "A":
       state["FirstPlayerSet"].append(last_card)
+      processFinished(gameinfo, state, "B", can_draw)
     else:
       state["SecondPlayerSet"].append(last_card)
+      processFinished(gameinfo, state, "A", can_draw)
     state["LastCard"] = None
   else:
     arr = None
@@ -42,13 +66,18 @@ def addEvent(gameinfo, pid, move_enc):
     else:
       arr = state["SecondPlayerSet"]
     card = move["Card"]
+    if card not in arr:
+      raise Exception("Player doesnt have that card")
     arr.remove(card)
     if state["LastCard"] == None:
       state["LastCard"] = card
       should_swap = True
     else:
-      if card < state["LastCard"]:
-        should_swap = True
+      if card > state["LastCard"]:
+        should_swap = False
+      else:
+        raise Exception("Cannot put a smaller card")
+      processFinished(gameinfo, state, "B" if pid == "A" else "A", can_draw)
       state["LastCard"] = None
   if should_swap:
     if pid == "A":
@@ -80,7 +109,7 @@ def main():
   try:
     result = callWrapper(request)
   except Exception as e:
-    print(json.dumps(str(e)))
+    print(json.dumps(str(e)), file=sys.stderr)
     sys.exit(1)
   print(json.dumps(result))
   
