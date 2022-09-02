3a4adb339eb68f54ac15f750cc84a9e8833160be
Sabiyat <kssabiyat@gmail.com>
2022-09-02 23:42:09 +0300

    Add weights to whistette interface

--- Website/src/components/GameData/SingleSuit/Whistette/WhistetteInterface.vue
+++ Website/src/components/GameData/SingleSuit/Whistette/WhistetteInterface.vue
@@ -2,20 +2,17 @@
   <b-container>
     <div v-if="game_state === null">
       <center>
-        {{$t('message.SingleSuitInterface.NullGameState')}}
+        {{ $t('message.SingleSuitInterface.NullGameState') }}
       </center>
     </div>
     <div v-else>
       <div class="container mt-2">
         <b-card class="text-center" :title="$t('message.WhistetteInterface.GameType')">
-          <div class="mt-2"> 
+          <div class="mt-2">
             <b-row class="justify-content-md-center" style="min-height: 80px">
               <b-col md="auto" v-for="c in game_state.first_player_set">
-                <b-button 
-                  class="card-button"
-                  :disabled="!is_active || game_state.current_player == 'B'"
-                  @click="makeMoveInterface({type:'move', card: c})"
-                >{{ c }}</b-button>
+                <b-button class="card-button" :disabled="!is_active || game_state.current_player == 'B'"
+                  @click="makeMoveInterface({ type: 'move', card: c })">{{ c }}</b-button>
               </b-col>
             </b-row>
 
@@ -25,11 +22,9 @@
             <br>
 
             <b-row class="justify-content-md-center" style="min-height: 80px">
-              <b-button class="card-button bg-secondary" variant="secondary"
-                v-if="null != game_state.last_card"
-                @click="makeMoveInterface({type:'move', card: c})"
-                >
-                  {{game_state.last_card}}
+              <b-button class="card-button bg-secondary" variant="secondary" v-if="null != game_state.last_card"
+                @click="makeMoveInterface({ type: 'move', card: c })">
+                {{ game_state.last_card }}
               </b-button>
             </b-row>
             <br>
@@ -39,26 +34,39 @@
 
             <b-row class="justify-content-md-center" style="min-height: 80px">
               <b-col md="auto" v-for="c in game_state.second_player_set">
-                <b-button 
-                  class="card-button"
-                  :disabled="!is_active || game_state.current_player == 'A'" 
-                  @click="makeMoveInterface({type:'move', card: c})"
-                >{{ c }}</b-button>
+                <b-button class="card-button" :disabled="!is_active || game_state.current_player == 'A'"
+                  @click="makeMoveInterface({ type: 'move', card: c })">{{ c }}</b-button>
               </b-col>
             </b-row>
 
             <br>
-            <br>
+
+            <b-card v-if="weighted_game">
+              <b-row class="justify-content-md-center">
+                <b-col md="auto">
+                  <b>{{ $t('message.SingleSuitSettings.Weights') }}:</b>
+                </b-col>
+                <b-col md="auto" v-for="(weight, index) in weights">
+                  <div v-if="current_bribe_number == index">
+                    <b>{{ weight }}</b>
+                  </div>
+                  <div v-else>
+                    {{ weight }}
+                  </div>
+                </b-col>
+              </b-row>
+            </b-card>
+
             <br>
 
             <div class="justify-content-md-center">
-              {{$t('message.Whistette.PlayerAScore')}}: {{game_state.first_score}}
+              {{ $t('message.Whistette.PlayerAScore') }}: {{ game_state.first_score }}
             </div>
 
             <div class="justify-content-md-center">
-              {{$t('message.Whistette.PlayerBScore')}}: {{game_state.second_score}}
+              {{ $t('message.Whistette.PlayerBScore') }}: {{ game_state.second_score }}
             </div>
-          </div>  
+          </div>
         </b-card>
       </div>
     </div>
@@ -66,10 +74,10 @@
 </template>
 
 <style scoped>
-  .card-button {
-   width:55px;
-   height:80px;
-  }
+.card-button {
+  width: 55px;
+  height: 80px;
+}
 </style>
 
 <script>
@@ -86,23 +94,42 @@ export default {
       is_active: false,
       game_type: "whistette",
       game_name: "Whistette",
+      weights: null,
+      current_bribe_number: 0,
     }
   },
-  methods: {
-    withWeights() {
+  computed: {
+    weighted_game() {
       return this.game_state.weights != null && this.game_state.weights.length > 0
     },
+  },
+  methods: {
     setState(state, game_type, player_id) {
       this.game_state = state
       this.game_type = game_type
       this.game_name = Whistette.getInterfaceGameName(game_type)
+
+      if (this.weighted_game && this.weights == null) {
+        this.weights = this.game_state.weights.slice(0, Math.ceil(this.game_state.weights.length / 2))
+      }
+
+      if (this.weighted_game) {
+        this.updateCurrentBribeNumber();
+      }
     },
     setIsActive(is_active) {
       this.is_active = is_active
     },
     makeMoveInterface(move) {
-      console.log("with weights: ", this.withWeights())
       this.$emit("move", move);
+    },
+    updateCurrentBribeNumber() {
+      for (let i = 0; i < this.weights.length; i++) {
+        if (this.game_state.weights[0] == this.weights[i]) {
+          this.current_bribe_number = i;
+          break;
+        }
+      }
     },
     applyMoveInterface(move) {
       // this.game_state = Whistette.applyMove(this.game_state, move);
